bf10025fde569873886ad45ce26b1aa77e6de3ba
Ramcuby <onlyu52@163.com>
2022-08-21 10:28:42 +0800

    ex43

--- ex43.py
+++ ex43.py
@@ -74,19 +74,87 @@ class CentralCorridor(Scene):
 
         elif action == 'dodge!':
             print(dedent("""
+                Like a world class boxer you dodge, weave, slip and
+                side right as the Gothon's blaster cranks a laser
+                past your head. In the middle of your artful dodge
+                your foot slips and you bang your head on the metal
+                wall and pass out. You wake up shortly after only to
+                die as the Gothon stomps on your head and eats you.
+                """))
+            return 'death'
 
-
-
+        elif action == 'tell a joke':
+            print(dedent("""
+                Lucky for you they made you learn Gothon insults in
+                the academy. You tell the one Gothon joke you know:
+                Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr,
+                fur fvgf nebhaq qur buhfr. The Gothon stops, tries
+                not to laugh, then busts out laughing and can't move.
+                While he's laughing you run up and shoot him square in
+                the head putting him down, then jump through the
+                Weapon Armory door.
             """))
+            return 'laser_weapon_armory'
+
+        else:
+            print("DOES NOT COMPUTE!")
+            return 'central_corridor'
+
 class LaserWeaponArmory(Scene):
 
-    def Enter(self):
-        pass
+    def enter(self):
+        print(dedent("""
+            You do a drive roll into the Weapon, crouch and scan
+            the room for more Gothons that might be hiding. It's dead
+            quiet, too quiet. You stand up and run to the far side of
+            the room and find the neutron bomb in its container.
+            There's a keypad lock on the box and you need the code to
+            get the bomb out. If you get the code wrong 10 times then
+            the lock closes forever and you can't get the bomb. The
+            code is 3 digit.
+            """))
+
+        code = f"{randint(1,9)}{randint(1,9)}{randint(1,9)}"
+        guess = input("[keypad]> ")
+        guesses = 0
+
+        while guess != code and guesses < 10:
+            print("BZZZZZZZZDDD!")
+            guesses +=1
+            guess = input("[keypad]> ")
+
+        if guess == code:
+            print(dedent("""
+                The container clicks open and the seal breaks, letting
+                gas out. You grab the neutron bomb and run as fast as
+                you can to the bridge where you must place it in the
+                right spot.
+                """))
+            return 'the_bridge'
+        else:
+            print(dedent("""
+                The lock buzzes one last time and then you hear a
+                sickening melting sound as the mechanism is fused
+                together. You decide to sit there, and finally the
+                Gothons blow up the ship from their ship and you die.
+                """))
+            return 'death'
 
 class TheBridge(Scene):
 
     def enter(Scene):
-        pass
+        print(dedent("""
+            You burst  onto the Bridge with the netron destruct bomb
+            under your arm and suprise 5 Gothons who are trying to
+            take control of the ship. Each of them has an even uglier
+            clown costume than the last. They haven't pulled their
+            weapons out yet, as they see the active bomb under your
+            arm and don't want you to set it off.
+            """))
+
+        action = input("> ")
+
+        
 
 class EscapePod(Scene):
 
